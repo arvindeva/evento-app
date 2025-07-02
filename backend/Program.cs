81c0b6bea1a6551dd13030c191f8f993389fad08
arvindeva <arvindeva@gmail.com>
2025-07-02 13:26:25 +0700

    Setup db connection

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,20 @@
+using Npgsql;
+
+DotNetEnv.Env.Load();
+var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+try
+{
+    using var conn = new NpgsqlConnection(connectionString);
+    conn.Open();
+    Console.WriteLine("✅ PostgreSQL connection successful.");
+    conn.Close();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"❌ PostgreSQL connection failed: {ex.Message}");
+}
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add required services (you'll use these later)
